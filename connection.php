71e30ebc0d4ff3616f4a97f2aa79d782d54d17b4
VishakhaDJadhav <vishakhajadhav231@gmail.com>
2019-12-29 18:34:14 +0530

    First upload

---
+++ connection.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "exam";
+
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+if($conn)
+{
+	echo "";
+}
+
+
+else
+{
+	echo "Connection failed";
+	die("connection failed because".mysqli_connect_error);
+}
+
+?>
